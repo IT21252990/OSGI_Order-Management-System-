b68c113fd027800b841373b0848c2f96e5dd0524
Sithara Pramodini <99314920+SitharaPramodini@users.noreply.github.com>
2024-03-14 22:13:18 +0530

    Delete FeedBackServicePublisher directory

--- FeedBackServicePublisher/src/feedbackservicepublisher/FeedbackService.java
+++
@@ -1,9 +0,0 @@
-package feedbackservicepublisher;
-
-public interface FeedbackService {
-	
-	public void giveFeedback(int CustomerID);
-	public void viewMyFeedback(int CustomerID);
-	public void deleteFeedback(int CustomerID);
-
-}
